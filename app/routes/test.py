011c660a955c8dc445af27a32c8280010902517e
GitUnni <jokerakashunni@gmail.com>
2026-05-09 13:19:45 +0530

    commented print statements

--- app/routes/test.py
+++ app/routes/test.py
@@ -5,9 +5,9 @@ from datetime import datetime
 load_dotenv("app/.env")
 sender_email = os.getenv("sender_email")
 sender_password = os.getenv("sender_password")
-print(sender_email)
-print(sender_password)
+# print(sender_email)
+# print(sender_password)
 
 TODAY        = datetime.now()
 TODAY_STR    = TODAY.strftime("%B %d, %Y")
-print(TODAY_STR)
+# print(TODAY_STR)
